337db12a91221462b806cc7cfe67dd9f4ccbc48d
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-06 20:09:52 -0700

    Add files via upload

---
+++ array practice/app.ts
@@ -0,0 +1,41 @@
+//Array
+let icecreamFlavors = ["chocolate", "kulfa", "pista", "cheesecake"]
+// console.log(icecreamFlavors);
+
+//a. for adding and subtracting elements from loop
+
+//1. push: for adding element
+icecreamFlavors.push("crunch")
+console.log(icecreamFlavors);
+//2. pop: for removing last element of an array
+icecreamFlavors.pop()
+console.log(icecreamFlavors);
+//3. shift: for removing first element of an array
+icecreamFlavors.shift()
+console.log(icecreamFlavors);
+//4. unshift: for adding an element in beginning of an array
+icecreamFlavors.unshift("blueberry")
+console.log(icecreamFlavors);
+//5. slice: to copy a part or any element in an array without changing it
+let someicecreamFlavors = icecreamFlavors.slice(1, 3)  //array mei jo element chahiye phele uske index no likho phir jaha tk elements extract krne hai us element ke baad ke element ka index no likho
+console.log(someicecreamFlavors);
+//6. splice: changes the array by removing or adding items
+let removeicecreamFlavor = icecreamFlavors.splice(1, 1, "crunch")  //array mei se jo element remove krna hai uska index no likho phir coma laga kr kitne no of elements delete krne hai wo likho phir agar kisi element ko replace krna hai to usse replace krne ke loye new element ka nam likho
+console.log(removeicecreamFlavor); 
+console.log(icecreamFlavors);
+
+//b. for reversing array
+icecreamFlavors.reverse()
+console.log(icecreamFlavors);
+
+//c. for counting number of elements in an array.    //same can be done using while & do-while loop
+console.log("Array length", icecreamFlavors.length); 
+for (let i = 0; i < icecreamFlavors.length; i++){
+   console.log(i);
+   console.log(icecreamFlavors[i]);
+}
+
+//d. loop for an array to arrange it line-wise      //same can be done using while & do-while loop
+for (let i = 0; i < 4; i++){
+    console.log(icecreamFlavors[i]);
+    }
---
+++ loop practice/app.js
@@ -0,0 +1,61 @@
+//for loop
+// for(let i = 0; i < 5; i++){
+// console.log("hello world", i);
+// }
+//practicing for loop
+// for(let i = 20; i < 30; i = i + 1){ //initialization mei jo number likhenge us number se loop/msg print hona start krega
+// console.log("warsa parhle", i);  //condition mei jo number likhenge utni times msg print hoga
+// }
+//how to add all the values in an array through for loop
+// let num1 = [10, 20, 30, 40]
+// let sum = 0 //sum of num1
+// for (let i = 0; i < num1.length; i++){ //i+=1 also gives the same result
+// sum += num1[i];
+// }
+// console.log(sum);
+//loop for an array altogether
+// let names = ["arin","billy","charlie","dory","eliana","fiona","gabriel"]
+// for (let i = 0; i < 7; i++){
+// console.log(names[i]);
+// }
+//if we dont write the same amount of names as mentioned in the condition number then it will give us undefined
+//for eg
+// let names1 = ["arin","billy","charlie"]
+// for (let i = 0; i < 5; i++){
+// console.log(names1[i]);
+// }
+//while loop: executes a block of code as long as the specified condition is true
+// agar 1 chez galat hogi to wo chal jayega phir error dega
+// isme steps line se honge or i++ bracket mei hoga with console.log
+// let i = 0
+// while(i < 10){
+// console.log("warsa plz parhle",i);
+// i++
+// }
+//how to add all the values in an array through while loop
+// let num1 = [10, 20, 30, 40];
+// let sum = 0;
+// let i = 0;
+// while (i < num1.length) {
+//   sum += num1[i];
+//   i++;
+// }
+// console.log(sum);
+//do-while loop
+// let i = 0
+// do{
+// console.log("warsa go study",i);
+// i++
+// } while(i < 10)
+//how to add all the values in an array through do-while loop
+// let num1 = [10, 20, 30, 50];
+// let sum = 0;
+// let i = 0;
+// do {
+//   sum += num1[i];
+//   i++;
+// } while (i < num1.length);
+// console.log(sum);
+var num = [10, 20, 30, 50];
+num.reverse();
+console.log(num);
